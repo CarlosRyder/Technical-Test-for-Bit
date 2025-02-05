46c532fcad7a4f00a5501c1364fbf5738d2141f0
Carlos Palacios <carlospalacios78@hotmail.com>
2025-02-05 03:36:27 -0500

    Update selected panel for UI

--- Assets/Scripts/ItemSlot.cs
+++ Assets/Scripts/ItemSlot.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class ItemSlot : MonoBehaviour
+public class ItemSlot : MonoBehaviour, IPointerClickHandler
 {
     public string itemName;
     public int quantity;
     public Sprite itemSprite;
     public bool isFull;
-
+    public GameObject selectedShader;
+    public bool isThisItemSelected;
+    private InventoryManager inventoryManager;
     [SerializeField] TMP_Text quantityText;
     [SerializeField] Image itemImage;
+
+    private void Awake()
+    {
+        inventoryManager = GameObject.Find("CanvasUI").GetComponent<InventoryManager>();
+    }
 
     public void AddItem(string itemName, int quantity, Sprite itemSprite) 
     {
@@ -22,5 +30,29 @@ public class ItemSlot : MonoBehaviour
         quantityText.text = quantity.ToString();
         quantityText.enabled = true;
         itemImage.sprite = itemSprite;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left) 
+        {
+            OnLeftClick();
+        }  
+        if (eventData.button == PointerEventData.InputButton.Right) 
+        {
+            OnRightClick();
+        }
+    }
+
+    public void OnLeftClick() 
+    {
+        inventoryManager.DeselectAllSlots();
+        selectedShader.SetActive(true);
+        isThisItemSelected = true;
+    }
+
+    public void OnRightClick() 
+    {
+        
     }
 }
